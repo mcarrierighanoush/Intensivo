b749e852ff60f9e58df30790e614cb7ad9077762
Mateus <mcarrieri70@gmail.com>
2023-12-14 19:26:03 -0300

    Adivinhe o numero

---
+++ jogoDoNumero.js
@@ -0,0 +1,18 @@
+
+var numeroSecreto = parseInt(Math.random() * 1000) + 1;
+var tentativas = 0;
+
+while(chute != numeroSecreto) {
+  var chute = prompt("Chute um número entre 1 e 1000");
+  tentativas++;
+
+  if (chute == numeroSecreto) {
+    alert("Você acertou!!");
+  } else if (chute < numeroSecreto) {
+    alert(`O número secreto é maior que ${chute}`);
+  } else if (chute > numeroSecreto) {
+    alert(`O número secreto é menor ${chute}`);
+  }
+}
+
+alert("Você deu " + tentativas + " tentativas.");
