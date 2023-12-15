76506027de2e8bf4f06695989a5c7beffef599cd
Mateus <mcarrieri70@gmail.com>
2023-12-15 09:09:59 -0300

    Atividade2

---
+++ Aula2.js
@@ -0,0 +1,12 @@
+var nome = prompt ("Qual o seu nome?");
+var idade = prompt ("Quantos anos você tem?");
+var linguagem = prompt ("Qual linguagem de programação você está estudando?");
+
+alert (`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
+
+var resposta = prompt (`você gosta de ${linguagem}? Responda sim ou não?`)
+if (resposta == "sim") {
+alert ("Muito bom! Continue estudando e você terá muito sucesso")
+} else {
+alert ("Ahh que pena... Já tentou aprender outras linguagens?")
+}
